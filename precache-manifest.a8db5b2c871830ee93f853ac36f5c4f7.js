f42240d6b83a05edc082dfa85140e488b781d715
Filip Volf <volf.filip@gmail.com>
2020-04-22 23:35:17 +0100

    Updates

---
+++ precache-manifest.a8db5b2c871830ee93f853ac36f5c4f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8969b68cc4c75d6346a185989e9b6bf2",
+    "url": "/kindly_front_end/index.html"
+  },
+  {
+    "revision": "d28cbd4ee481520d2817",
+    "url": "/kindly_front_end/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "11e23d2552e2939c74b3",
+    "url": "/kindly_front_end/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "d28cbd4ee481520d2817",
+    "url": "/kindly_front_end/static/js/2.5fac0129.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/kindly_front_end/static/js/2.5fac0129.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11e23d2552e2939c74b3",
+    "url": "/kindly_front_end/static/js/main.a544d840.chunk.js"
+  },
+  {
+    "revision": "57a48cdf08c107c72fe4",
+    "url": "/kindly_front_end/static/js/runtime-main.0bb26529.js"
+  }
+]);
