15a9160ff3bab652d3e43249b761df4f1db40ad7
Filip Volf <volf.filip@gmail.com>
2020-04-27 22:44:52 +0100

    add feedback form

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom"
 import INeedHelp from "./pages/INeedHelp"
 import IWantToHelp from "./pages/IWantToHelp"
 import LandingPage from "./pages/LandingPage"
-import NavBar from "./components/NavBar"
+import Feedback from "./components/Feedback"
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
       <Route path="/" exact component={LandingPage} />
       <Route path="/ineedhelp" component={INeedHelp} />
       <Route path="/iwanttohelp" component={IWantToHelp} />
+      <Route path="/feedback" component={Feedback} />
     </Router>
   )
 }
---
+++ src/components/Feedback.js
@@ -0,0 +1,62 @@
+import React from "react"
+import { withFormik, Form, Field } from "formik"
+import * as Yup from "yup"
+import { Button, Label, FormGroup } from "reactstrap"
+import { ReactstrapInput } from "reactstrap-formik"
+
+const Feedback = ({ errors, touched }) => {
+  return (
+    <Form className="askhelp-form">
+      <FormGroup>
+        <Label>Email</Label>
+        <Field
+          component={ReactstrapInput}
+          type="text"
+          name="email"
+          placeholder="Email"
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label>Předmět</Label>
+        <Field
+          component={ReactstrapInput}
+          type="text"
+          name="predmet"
+          placeholder="Předmět"
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label>Popis</Label>
+        <Field
+          component={ReactstrapInput}
+          type="textarea"
+          rows="5"
+          name="popis"
+          placeholder="Popis"
+        />
+      </FormGroup>
+
+      <Button className="btn-lg btn-dark btn-block">Submit</Button>
+    </Form>
+  )
+}
+
+export default withFormik({
+  validationSchema: Yup.object().shape({
+    predmet: Yup.string()
+      .matches(/.+\s.+/)
+      .required("Chybí předmět")
+      .max(100, "Předmět je příliš dlouhý"),
+    email: Yup.string()
+      .required("Email je povinný")
+      .email("Neplatný email")
+      .max(100, "Email je příliš dlouhý"),
+    popis: Yup.string()
+      .min(50, "Popis je příliš krátký")
+      .max(1000, "Popis je příliš dlouhý")
+      .required("Chybí popis"),
+  }),
+  handleSubmit(values) {
+    console.log("Request should be send with these values:", values)
+  },
+})(Feedback)
