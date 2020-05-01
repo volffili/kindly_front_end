e89fddd7b3c8453147bb631273e58f23e12ecb7b
Tomas Ye <tomaye@amazon.com>
2020-05-01 11:21:04 +0200

    [ASK_HELP_FORM] 1) Improved the look of the form component 2) Translated the form controls into Czech 3) Connected the form to the Google Maps Autocomplete component

--- src/components/AskHelp.js
+++ src/components/AskHelp.js
@@ -1,10 +1,9 @@
 import React, {useState} from 'react';
-import {withFormik, Form, Field} from 'formik';
+import {withFormik, Form, Field, useFormik} from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
 import {baseUrl} from '../Url';
 import {Button, Label, FormGroup} from 'reactstrap';
-import { ReactstrapInput } from "reactstrap-formik";
 import LocationInput from "./LocationAutocomplete";
 import  "./style.css";
 
@@ -16,64 +15,133 @@ const AskHelp = ({
     const [address, setAddress] = useState({});
 
     const handleAddressChange = address_event => {
-        setAddress(address_event)
+        setAddress(address_event);
     };
 
 
-    return<div className='form-wrapper'>
-        <Form className="askhelp-form">
-            <FormGroup>
-                <Label>Jméno</Label>
-                <Field component={ReactstrapInput} type="text" name="name" placeholder="Křestní jméno a příjmení"/>
-            </FormGroup>
-            <FormGroup>
-                <Label>Věk</Label>
-                <Field component={ReactstrapInput} type="text" name="age" placeholder="Počet roků"/>
-            </FormGroup>
-            <FormGroup>
-                <Label>Telefonní číslo</Label>
-                <Field component={ReactstrapInput} type="text" name="phone" placeholder="Ve formátu 123456789"/>
-            </FormGroup>
-            <FormGroup>
-                <Label>Adresa</Label>
-                <LocationInput onAddressSelect={handleAddressChange}/>
-            </FormGroup>
-            <FormGroup>
-                <Label>Předmět žádosti</Label>
-                <Field component={ReactstrapInput} type="text" name="topic" placeholder="Např. Venčení psa"/>
-            </FormGroup>
-            <FormGroup>
-                <Label>Popis žádosti</Label>
-                <Field component={ReactstrapInput} type="textarea" rows="3" name="description" placeholder="Např. Mám 2 psy typu čivava které je potřeba venčit 2-krát denně, ráno a večer"/>
-            </FormGroup>
-
-            <Button className="btn-lg btn-dark btn-block">Submit</Button>
-        </Form>
-    </div>
-};
-
-export default withFormik({
-    validationSchema: Yup.object().shape({
-        name: Yup.string().required(),
-        age: Yup.number().min(0).max(200).required(),
-        phone: Yup.string().required(),
-        address: Yup.string().required(),
-        topic: Yup.string().required(),
-        description: Yup.string().required()
-    }),
-    handleSubmit(values){    
-        console.log('values = ', values);
-        console.log('address = ', address);
-        axios.get(baseUrl,{ 
-            params: { 
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            age: '',
+            phone_number: '',
+            request_topic: '',
+            request_description: ''
+        },
+        onSubmit: values => {
+            let query_params = {
                 api_name: "post_help_request",
                 requester_name: values.name,
                 requester_age: values.age,
-                requester_phone_number: values.phone,
-                requester_address: values.address,
-                request_topic: values.topic,
-                request_details: values.description
-            }
-        })
-    }
+                requester_phone_number: values.phone_number,
+                requester_address: address.formatted_address,
+                requester_address_lat: address.lat,
+                requester_address_lng: address.lng,
+                request_topic: values.request_topic,
+                request_details: values.request_description
+            };
+            console.log('Query Params = ', query_params)
+
+            axios.get(baseUrl,{
+                params: query_params
+            })
+
+        },
+    });
+
+
+    return (
+        <div className='form-wrapper'>
+            <Form className="askhelp-form" onSubmit={formik.handleSubmit}>
+                <FormGroup>
+                    <div className="input-label">
+                        <span >Jméno</span>
+                    </div>
+                    <div>
+                        <input
+                            id="name"
+                            name="name"
+                            type="text"
+                            placeholder="Křestní jméno a příjmení"
+                            onChange={formik.handleChange}
+                            value={formik.values.name}
+                        />
+                    </div>
+                </FormGroup>
+                <FormGroup>
+                    <div className="input-label">
+                        <span >Věk</span>
+                    </div>
+                    <div>
+                        <input
+                            id="age"
+                            name="age"
+                            type="text"
+                            placeholder="Počet roků"
+                            onChange={formik.handleChange}
+                            value={formik.values.age}
+                        />
+                    </div>
+                </FormGroup>
+                <FormGroup>
+                    <div className="input-label">
+                        <span >Telefonní číslo</span>
+                    </div>
+                    <div>
+                        <input
+                            id="phone_number"
+                            name="phone_number"
+                            type="text"
+                            placeholder="Ve formátu 720123789"
+                            onChange={formik.handleChange}
+                            value={formik.values.phone_number}
+                        />
+                    </div>
+                </FormGroup>
+                <FormGroup>
+                    <div className="input-label">
+                        <span >Adresa</span>
+                    </div>
+                    <LocationInput onAddressSelect={handleAddressChange}/>
+                </FormGroup>
+                <FormGroup>
+                    <div className="input-label">
+                        <span >Předmět žádosti</span>
+                    </div>
+                    <div>
+                        <input
+                            id="request_topic"
+                            name="request_topic"
+                            type="text"
+                            placeholder="Např. Venčení psa"
+                            onChange={formik.handleChange}
+                            value={formik.values.request_topic}
+                        />
+                    </div>
+                </FormGroup>
+                <FormGroup>
+                    <div className="input-label">
+                        <span >Popis žádosti</span>
+                    </div>
+                    <div>
+                        <input
+                            id="request_description"
+                            name="request_description"
+                            type="text"
+                            placeholder="Např. Mám 2 psy typu čivava které je potřeba venčit 2-krát denně, ráno a večer"
+                            onChange={formik.handleChange}
+                            value={formik.values.request_description}
+                        />
+                    </div>
+                </FormGroup>
+
+                <Button className="btn-lg btn-dark btn-block submit-button"  type="submit">Poslat žádost</Button>
+            </Form>
+        </div>
+    );
+
+
+
+};
+
+export default withFormik({
 })(AskHelp)
