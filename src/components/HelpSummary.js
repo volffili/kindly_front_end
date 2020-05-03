24a4371f00410aac357a8c467d47d11e96c8bf6c
Tomas Ye <tomaye@amazon.com>
2020-05-03 11:49:06 +0200

    [I_WANT_TO_HELP_PAGE] Adding links to the RequestDetails both from the Map and from the list of requests

--- src/components/HelpSummary.js
+++ src/components/HelpSummary.js
@@ -1,11 +1,17 @@
 import React, {useEffect} from "react"
 import { Button } from "reactstrap"
+import { useHistory } from 'react-router-dom';
 
 export default (props) => {
+  const history = useHistory();
 
   useEffect(() => {
   }, []);
 
+  const redirectToDetails = () => {
+      const url = `/details/${props.helpRequest.request_id}`;
+      window.open(url);
+  };
 
   return (
     <tr>
@@ -13,7 +19,7 @@ export default (props) => {
       <td>{props.helpRequest.request_topic}</td>
       <td>{props.helpRequest.requester_address}</td>
       <td>
-        <Button color="success" size="sm">
+        <Button color="success" onClick={redirectToDetails}>
           Více
         </Button>
       </td>
--- src/components/Map.js
+++ src/components/Map.js
@@ -6,6 +6,7 @@ import {
   InfoWindow,
 } from "react-google-maps"
 import React, { useEffect, useState } from "react"
+import {Link} from "react-router-dom";
 
 const Map =
   withGoogleMap((props) => {
@@ -42,12 +43,13 @@ const CustomMarker = (props) => {
         <InfoWindow options={{ closeBoxURL: ``, enableEventPropagation: true }}>
           <div>
             <h3>{props.helpRequest.request_topic}</h3>
-            <span>{props.helpRequest.request_details}</span>
+            <h5>{props.helpRequest.request_details}</h5>
+            <h6>Více informaci <Link to={`/details/${props.helpRequest.request_id}`} target="_blank">zde</Link></h6>
           </div>
         </InfoWindow>
       )}
     </Marker>
   )
-}
+};
 
 export default Map
