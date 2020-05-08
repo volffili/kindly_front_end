184a3bf3af35ef7154ca51e8deeebd88d231dda0
Filip Volf <volf.filip@gmail.com>
2020-05-09 00:08:56 +0100

    use hooks in LocationAutocomplete + add styles + reformat

--- src/components/LocationAutocomplete.js
+++ src/components/LocationAutocomplete.js
@@ -1,95 +1,71 @@
 import PlacesAutocomplete from "react-places-autocomplete"
-import {
-  geocodeByAddress,
-  getLatLng,
-} from "react-places-autocomplete"
-import * as React from "react"
-import "./style.css"
+import { geocodeByAddress, getLatLng } from "react-places-autocomplete"
+import React, { useState } from "react"
+import { ListGroup, ListGroupItem } from "reactstrap"
+import styled from "styled-components/macro"
 
-class LocationInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      address: "",
-      lat: "",
-      lng: "",
-    };
-  }
+const Wrap = styled.div`
+  position: relative;
+`
 
-  handleChange = (address) => {
-    this.setState({
-      address: address,
-    })
-  };
+const ListGroupOnTop = styled(ListGroup)`
+  position: absolute;
+  z-index: 1000;
+`
+const HandCursorListGroupItem = styled(ListGroupItem)`
+  cursor: pointer;
+`
 
-  handleSelect = (address) => {
-    this.setState({
-      address: address,
-    });
+export default (props) => {
+  const [address, setAddress] = useState("")
+
+  const handleSelect = (address) => {
+    setAddress(address)
     geocodeByAddress(address)
       .then((results) => getLatLng(results[0]))
       .then((latLng) => {
-        this.setState(latLng)
-        this.props.onAddressSelect({
-          formatted_address: this.state.address,
-          lat: this.state.lat,
-          lng: this.state.lng,
+        props.onAddressSelect({
+          formatted_address: address,
+          lat: latLng.lat,
+          lng: latLng.lng,
         })
       })
       .catch((error) => console.error("Error", error))
-  };
+  }
 
-  searchOptions = {
+  const searchOptions = {
     //Prague location
     location: new window.google.maps.LatLng(49.8600624, 15.5860745),
     radius: 100000,
     types: ["address"],
-  };
-
-  render() {
-    return (
-      <PlacesAutocomplete
-        value={this.state.address}
-        onChange={this.handleChange}
-        onSelect={this.handleSelect}
-        searchOptions={this.searchOptions}
-      >
-        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-          <div>
-            <input
-              {...getInputProps({
-                placeholder:
-                  "Začněte psát jmeno ulice, my za Vás doplníme zbytek      (nepovinné pole)",
-                className: "location-search-input address-input",
-              })}
-            />
-            <div className="autocomplete-dropdown-container">
-              {loading && <div>Loading...</div>}
-              {suggestions.map((suggestion) => {
-                const className = suggestion.active
-                  ? "suggestion-item--active"
-                  : "suggestion-item";
-                // inline style for demonstration purpose
-                const style = suggestion.active
-                  ? { backgroundColor: "#fafafa", cursor: "pointer" }
-                  : { backgroundColor: "#ffffff", cursor: "pointer" }
-                return (
-                  <div
-                    {...getSuggestionItemProps(suggestion, {
-                      className,
-                      style,
-                    })}
-                  >
-                    <span>{suggestion.description}</span>
-                  </div>
-                )
-              })}
-            </div>
-          </div>
-        )}
-      </PlacesAutocomplete>
-    )
   }
-}
 
-export default LocationInput
+  return (
+    <PlacesAutocomplete value={address} onChange={setAddress} onSelect={handleSelect} searchOptions={searchOptions}>
+      {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+        <Wrap>
+          <input
+            {...getInputProps({
+              placeholder: "Začněte psát jmeno ulice, my za Vás doplníme zbytek      (nepovinné pole)",
+              className: "location-search-input address-input",
+            })}
+          />
+          <ListGroupOnTop>
+            {loading && <HandCursorListGroupItem>Loading...</HandCursorListGroupItem>}
+            {suggestions.map((suggestion) => {
+              return (
+                <HandCursorListGroupItem
+                  key="suggestion"
+                  color={suggestion.active && "info"}
+                  {...getSuggestionItemProps(suggestion)}
+                >
+                  <span>{suggestion.description}</span>
+                </HandCursorListGroupItem>
+              )
+            })}
+          </ListGroupOnTop>
+        </Wrap>
+      )}
+    </PlacesAutocomplete>
+  )
+}
--- src/pages/INeedHelp.js
+++ src/pages/INeedHelp.js
@@ -1,4 +1,157 @@
-import React from "react"
-import AskHelp from "../components/AskHelp"
+import React, { useState } from "react"
+import { Field, Form, Formik } from "formik"
+import * as Yup from "yup"
+import axios from "axios"
+import { baseUrl } from "../Url"
+import { Button, FormGroup, Label } from "reactstrap"
+import LocationInput from "../components/LocationAutocomplete"
+import Instructions from "../components/Instructions"
+import { ReactstrapInput } from "reactstrap-formik"
 
-export default () => <AskHelp />
+export default () => {
+  const [address, setAddress] = useState({})
+
+  const handleAddressChange = (address_event) => {
+    setAddress(address_event)
+    console.log("setting: ", address_event)
+  }
+
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().required("Jméno je povinné pole"),
+    age: Yup.number().typeError("Věk musí být číslo"),
+    email: Yup.string().email("Neplatný email. Systém očekává formát honza.novak@seznam.cz"),
+    phone_number: Yup.string().required("Telefonní číslo je povinné pole"),
+    request_topic: Yup.string().required("Předmět je povinné pole"),
+    request_description: Yup.string().required("Popis žádosti je povinné pole"),
+  })
+
+  const initialValues = {
+    name: "",
+    age: "",
+    phone_number: "",
+    email: "",
+    request_topic: "",
+    request_description: "",
+  }
+
+  const onSubmit = (values) => {
+    let query_params = {
+      api_name: "post_help_request",
+      requester_name: values.name,
+      requester_age: values.age,
+      requester_email: values.email,
+      requester_phone_number: values.phone_number,
+      requester_address: address.formatted_address,
+      requester_address_lat: address.lat,
+      requester_address_lng: address.lng,
+      request_topic: values.request_topic,
+      request_details: values.request_description,
+    }
+    console.log("Query Params = ", query_params)
+
+    axios.get(baseUrl, {
+      params: query_params,
+    })
+  }
+
+  return (
+    <div>
+      <Instructions
+        header="Čím Vám ostatní mohou pomoci?"
+        textRows={["Lorem ipsumdsfsdfsaf", "sadmfosdanfsdofnsdfn"]}
+      />
+      <div
+        style={{
+          width: "50%",
+          marginLeft: "25%",
+        }}
+      >
+        <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+          {({ errors, touched }) => (
+            <Form className="askhelp-form">
+              <FormGroup>
+                <Label htmlFor="name">Jméno</Label>
+                <Field
+                  id="name"
+                  name="name"
+                  component={ReactstrapInput}
+                  type="text"
+                  placeholder="Křestní jméno a příjmení  (povinné pole)"
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="age">Věk</Label>
+                <div>
+                  <Field id="age" name="age" type="text" placeholder="Počet roků   (nepovinné pole)" />
+                </div>
+                {errors.age && touched.age ? <div className="validation-error">{errors.age}</div> : null}
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="email">Email</Label>
+                <div>
+                  <Field
+                    id="email"
+                    name="email"
+                    type="text"
+                    placeholder="Ve formátu honza@novak.cz     (nepovinné pole)"
+                  />
+                </div>
+                {errors.email && touched.email ? <div className="validation-error">{errors.email}</div> : null}
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="phone_number">Telefonní číslo</Label>
+                <div>
+                  <Field
+                    id="phone_number"
+                    name="phone_number"
+                    type="text"
+                    placeholder="Ve formátu 720123789      (povinné pole)"
+                  />
+                </div>
+                {errors.phone_number && touched.phone_number ? (
+                  <div className="validation-error">{errors.phone_number}</div>
+                ) : null}
+              </FormGroup>
+              <FormGroup>
+                <Label>Adresa</Label>
+                <LocationInput onAddressSelect={handleAddressChange} />
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="request_topic">Předmět žádosti</Label>
+                <div>
+                  <Field
+                    id="request_topic"
+                    name="request_topic"
+                    type="text"
+                    placeholder="Např. Venčení psa   (povinné pole)"
+                  />
+                </div>
+                {errors.request_topic && touched.request_topic ? (
+                  <div className="validation-error">{errors.request_topic}</div>
+                ) : null}
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="request_description">Popis žádosti</Label>
+                <div>
+                  <Field
+                    id="request_description"
+                    name="request_description"
+                    type="text"
+                    placeholder="Např. Mám 2 psy typu čivava které je potřeba venčit 2-krát denně, ráno a večer   (povinné pole)"
+                  />
+                </div>
+                {errors.request_description && touched.request_description ? (
+                  <div className="validation-error">{errors.request_description}</div>
+                ) : null}
+              </FormGroup>
+
+              <Button className="btn-lg btn-dark btn-block submit-button" type="submit">
+                Poslat žádost
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
+  )
+}
