f9f6d96befeaeed8646d04c403b85f699f509257
Filip Volf <volf.filip@gmail.com>
2020-05-08 18:57:58 +0100

    landing page buttons info

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom"
 import { ReactSVG } from "react-svg"
 import lovepalms_svg from "../lovepalms_minified.svg"
 import lovepalms_jpg from "../lovepalms.jpg"
-import { Row, Col, Container } from "reactstrap"
+import { Row, Col, Container, Card, CardFooter, CardBody } from "reactstrap"
 
 const Img100 = styled.img`
   width: 100%;
@@ -19,26 +19,43 @@ export default () => (
   <Wrapper fluid={true}>
     <Row className="justify-content-center">
       <Col sm="10" className="my-auto p-5" align="center">
-        <h2>
+        <h1>Kindly</h1>
+        <h2>Vzájemná pomoc sousedů</h2>
+        <h4>
           Tady bude uvodni text o Kindly, co to je atd.! bal bla Kindly je super a diky nemu muzete pomoct tem kteri to
           nejvic potrebuji. Pomozte hned ted!
-        </h2>
+        </h4>
       </Col>
     </Row>
-    <Row>
-      <Col sm="6" align="center">
-        <Link to="/iwanttohelp">
-          <Button className="my-5" outline color="secondary" size="lg">
-            Nabízím pomoc
-          </Button>
-        </Link>
+    <Row className="justify-content-center">
+      <Col sm="6" xl="4" align="center">
+        <Card className="my-5" style={{ maxWidth: 400 }}>
+          <CardBody>
+            Máte trochu času a chcete udělat dobrý skutek? Podívejte se s čím potřebují pomoci lidé ve vašem okolí.
+          </CardBody>
+          <CardFooter>
+            <Link to="/iwanttohelp">
+              <Button outline color="secondary" size="lg">
+                Nabídnout pomoc
+              </Button>
+            </Link>
+          </CardFooter>
+        </Card>
       </Col>
-      <Col sm="6" className="my-auto" align="center">
-        <Link to="/ineedhelp">
-          <Button className="my-5" outline color="secondary" size="lg">
-            Žádám o pomoc
-          </Button>
-        </Link>
+      <Col sm="6" xl="4" className="my-auto" align="center">
+        <Card className="my-5" style={{ maxWidth: 400 }}>
+          <CardBody>
+            Potřebujete donést nákup, doprovodit k lékaři, vyvenčit psa nebo třeba pomoc s technikou? Nejbote se požádat
+            ty, co jsou vám nejblíže.
+          </CardBody>
+          <CardFooter>
+            <Link to="/ineedhelp">
+              <Button outline color="secondary" size="lg">
+                Požádat o pomoc
+              </Button>
+            </Link>
+          </CardFooter>
+        </Card>
       </Col>
     </Row>
     <ReactSVG
@@ -53,11 +70,11 @@ export default () => (
     />
     <Row className="justify-content-center">
       <Col sm="10" className="my-auto p-5" align="center">
-        <h3>
+        <h4>
           Tady je nejaky dalsi text. Tohle jsou nasi sponzori. Bla bla bla bla bla bla.{" "}
           <a href="/feedback">Muzete nam taky pomoct!</a> Dalsi kecy bla bla bla bla. Pomahejte si. Tecka. Lorem Ipsum.
           Cau Tommy, nechces pomoct vykopat nejaky kameni?
-        </h3>
+        </h4>
       </Col>
     </Row>
   </Wrapper>
